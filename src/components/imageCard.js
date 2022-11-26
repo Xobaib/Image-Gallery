5c3871ccf7aa46fa21fed7ed13d30cfec10096f7
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2022-11-26 03:41:27 +0330

    first commit

---
+++ src/components/imageCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
+
+  return (
+    // Main container
+    <div className="max-w-xs rounded overflow-hidden shadow-lg">
+      {/* part 1 */}
+      <img src={image.webformatURL} alt="" className="w-full h-80" />
+      {/* part 2 */}
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      {/* part 3 */}
+      <div className="px-6 py-4">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
